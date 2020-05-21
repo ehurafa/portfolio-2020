ca699ac64e9763c84297ed8c5bbe532439838ddc
Rafael Gomes <rafarfl2010@gmail.com>
2020-05-21 17:19:22 -0300

    🙈 creat template structure and cards init

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SharedComponent } from './shared/shared.component';
+import { ContainerComponent } from './container/container.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    SharedComponent,
+    ContainerComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
