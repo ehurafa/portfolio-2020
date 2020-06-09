78a95e0d9b53af4df10aeef94cb347f3b11799af
Rafael Gomes <rafarfl2010@gmail.com>
2020-06-09 15:09:32 -0300

    :hammer: refactory animations feedback

--- portfolio/src/app/pages/contact/contact.component.ts
+++ portfolio/src/app/pages/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -10,11 +10,35 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
 
+  testeeee = false;
+
+  validateTable = [
+     { "name": '', "valid": false, "typed": false},
+     { "email": '', "valid": false, "typed": false},
+     { "subject": '', "valid": false, "typed": false},
+     { "message": '', "valid": false, "typed": false}   
+  ];
+
   contactForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    subject: new FormControl(''),
-    message: new FormControl(''),
+
+      'name': new FormControl(this.validateTable[0]['name'], [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'email': new FormControl(this.validateTable[1]['email'], [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.email
+      ]),
+      'subject': new FormControl(this.validateTable[2]['subject'], [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      'message': new FormControl(this.validateTable[3]['message'], [
+        Validators.required,
+        Validators.minLength(4)
+      ])
+
   });
 
   completeSend = false;
@@ -26,22 +50,11 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  /*
-  onSubmit(contactForm) {
-    if (contactForm.valid) {
-      const email = contactForm.value;
-      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-      this.http.post('https://formspree.io/mlepjazv',
-        { name: email.name, replyto: email.email, message: email.messages },
-        { 'headers': headers }).subscribe(
-          response => {
-            console.log(response);
-          }
-        );
-    }
-  }
-*/
+  get name() { return this.contactForm.get('name'); }
+  get email() { return this.contactForm.get('email'); }
+  get subject() { return this.contactForm.get('subject'); }
+  get message() { return this.contactForm.get('message'); }
+ 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.log(this.contactForm.value);
@@ -53,7 +66,8 @@ export class ContactComponent implements OnInit {
       const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
       this.http.post('https://formspree.io/mlepjazv',
         { name: email.name, 
-          email: email.email, subject: email.subject, message: email.message 
+          //email: email.email, subject: email.subject, 
+          message: email.message 
         },
         { 'headers': headers }).subscribe(
           response => {
@@ -62,11 +76,12 @@ export class ContactComponent implements OnInit {
 
             setTimeout(function(){
               self.initFeedback = true;
-            }, 1000);
+            }, 200);
 
             setTimeout(function(){
               self.onFeedback = true;
               self.completeSend = false;
+            
             }, 2000);
 
             setTimeout(function(){
@@ -74,7 +89,7 @@ export class ContactComponent implements OnInit {
               self.onFeedback = false;
               self.initFeedback = false;
              
-            }, 5000);
+            }, 4000);
           }
         );
     }
