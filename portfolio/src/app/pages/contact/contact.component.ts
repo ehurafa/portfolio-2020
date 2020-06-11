ba87ceb3877ca1093c4d41bc57f05e66f5f5015b
Rafael Gomes <rafarfl2010@gmail.com>
2020-06-10 23:10:05 -0300

    :traffic_light: starting form steps config

--- portfolio/src/app/pages/contact/contact.component.ts
+++ portfolio/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, OnChanges, HostBinding } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { FormGroup, FormControl, Validators, NgForm, FormGroupDirective } from '@angular/forms';
 
@@ -8,9 +8,16 @@ import { FormGroup, FormControl, Validators, NgForm, FormGroupDirective } from '
   styleUrls: ['./contact.component.scss'],
 
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit, OnChanges {
 
-  testeeee = false;
+  progress = 0;
+  total = 100;
+  steps = 0;
+  completeSend = false;
+  initFeedback = false;
+  onFeedback = false;
+  color: string;
+  completeSteps: boolean = false;
 
   validateTable = [
      { "name": '', "valid": false, "typed": false},
@@ -41,31 +48,80 @@ export class ContactComponent implements OnInit {
 
   });
 
-  completeSend = false;
-  initFeedback = false;
-  onFeedback = false;
+  
 
   constructor(private http: HttpClient) { }
 
+  colors(): void {
+    this.completeSteps = false;
+   if(this.progress < 40) {
+     this.color = 'red';
+   } else if(this.progress < 60) {
+     this.color = 'orange';   
+   
+   } else if(this.progress < 90) {
+     this.color = 'green';
+   }
+   else {
+   this.color = 'blue';
+    this.completeSteps = true;
+   }
+ }
+
+  step(el, valid) {
+    this.completeSteps = false;
+  
+    if(el == 'name' && valid == true) {
+     
+      this.progress = 20;
+      this.color = 'red';
+    }  if(el == 'email' && valid == true) {
+     
+      this.progress = 40;
+      this.color = 'orange';   
+    } if(el == 'subject' && valid == true) {
+      this.progress = 60;
+      this.color = 'green';
+    } 
+     if(el == 'message' && valid == true) {
+      this.progress = 100;
+      this.color = 'blue';
+      this.completeSteps = true;
+    } 
+  
+  }
 
   ngOnInit(): void {
+
+    if(!this.progress) {
+      this.progress = 0;
+    }
+
+    if(this.total === 0) {
+      this.total = this.progress;
+    } else if(!this.total) {
+      this.total = 100;
+    }
+
+    if(this.progress > this.total) {
+      this.progress = 100;
+      this.total = 100;
+    }
+    this.progress = (this.progress / this.total) * 100;
+     
+    this.colors()
+ 
+
   }
   get name() { return this.contactForm.get('name'); }
   get email() { return this.contactForm.get('email'); }
   get subject() { return this.contactForm.get('subject'); }
   get message() { return this.contactForm.get('message'); }
-
-    
- 
-  onSubmit(formData: any, formDirective: FormGroupDirective ): void {
-
-    
+     
+  onSubmit(formData: any, formDirective: FormGroupDirective ): void {    
 
     // TODO: Use EventEmitter with form value
-    console.log(this.contactForm.value);
-
     let self = this;
-
 
     if (this.contactForm.valid) {
       const email = this.contactForm.value;
@@ -77,8 +133,7 @@ export class ContactComponent implements OnInit {
         },
         { 'headers': headers }).subscribe(
           response => {
-            this.completeSend = true;
-            console.log(response);
+            this.completeSend = true;           
 
             setTimeout(function(){
               self.initFeedback = true;
@@ -104,5 +159,8 @@ export class ContactComponent implements OnInit {
     }
   }
 
+  ngOnChanges () { 
+    this.colors();
+  }
 
 }
